83950c754fceab3d481b092f56aeffbded007df7
maddenlockin <maddenlockin@gmail.com>
2022-03-16 17:16:03 -0700

    add friend function complete

--- app.js
+++ app.js
@@ -43,25 +43,41 @@ addMushroomButton.addEventListener('click', () => {
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
     // create a new friend object
+    const newFriend = {
+        name: friendInputEl.value,
+        satisfaction: 2
+    };
     // push it into the friends state array, passed in as an argument
+    friendData.push(newFriend);
     // reset the input
+    friendInputEl.value = '';
     // display all the friends (use a function here)
+    displayFriends();
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsEl.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
+        const friendEl = renderFriend(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
-        //         and if the friend's satisfaction level is below 3 and you have mushrooms left
-        //             increment the friends satisfaction and decrement your mushrooms
-        //             then display your friends and mushrooms with the updated state
+        friendEl.addEventListener('click', () => {
 
+            //         and if the friend's satisfaction level is below 3 and you have mushrooms left
+            if (friend.satisfaction < 3 && mushroomCount > 0) {
+                //             increment the friends satisfaction and decrement your mushrooms
+                friend.satisfaction++; 
+                mushroomCount--;
+                //             then display your friends and mushrooms with the updated state
+            }
+            displayFriends();
+            displayMushrooms();
+        });
         // append the friendEl to the friends list in DOM
+        friendsEl.append(friendEl);
     }
 }
 
